b8ea2449223361c051f8cc62ecf8f584fa982799
Vipin9794 <rikyverma112@gmail.com>
2024-10-07 21:49:26 +0530

    cd

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,14 @@
-
+ // map.js
   mapboxgl.accessToken = mapToken;
    
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         style : "mapbox://styles/mapbox/streets-v12",
-        center: [77.209 ,28.6139], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
 
-    console.log(coordinates);
+    
 
     const marker = new mapboxgl.Marker()
     .setLngLat(coordinates)
